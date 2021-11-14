b9152128422db71188fb48cdfc41221ec2211932
lautaroLD <lauttyd@gmail.com>
2021-11-13 23:58:09 -0300

    agrego varios elementos

--- code.js
+++ code.js
@@ -13,7 +13,34 @@ let btn_cart_container = document.getElementById('cart-container')
 let second_section_cart = document.getElementById('second-section-cart')
 let cart_empty_msj = document.getElementById('cart-empty-msj')
 let btn_bx_trash = document.getElementById('bx-trash')
+let img_container_desktop = document.getElementsByClassName('imgs')
+let user_icon = document.getElementById('user-icon')
+let user_section_container = document.getElementById('user-section-container')
+
 // btn
+user_icon.addEventListener('click', () => {
+    if (user_section_container.className == 'user-section') {
+        user_section_container.classList.replace('user-section', 'user-section-disable')
+    } else {
+        if (cart_items_container.className == 'cart-items-container') {
+            cart_items_container.classList.replace('cart-items-container', 'cart-items-container-disable')
+        }
+        user_section_container.classList.replace('user-section-disable', 'user-section')
+    }
+})
+img_container_desktop[0].addEventListener('click', () => {
+    picture_article.className = 'back-img1'
+})
+img_container_desktop[1].addEventListener('click', () => {
+    picture_article.className = 'back-img2'
+})
+img_container_desktop[2].addEventListener('click', () => {
+    picture_article.className = 'back-img3'
+})
+img_container_desktop[3].addEventListener('click', () => {
+    picture_article.className = 'back-img4'
+})
+
 btn_bx_trash.addEventListener('click', () => {
     second_section_cart.classList.replace('second-section-cart', 'second-section-cart-disable')
     cart_empty_msj.classList.replace('cart-empty-msj-disable', 'cart-empty-msj')
@@ -24,6 +51,9 @@ btn_bx_trash.addEventListener('click', () => {
 
 btn_cart_container.addEventListener('click', () => {
     if (cart_items_container.className == 'cart-items-container-disable') {
+        if (user_section_container.className == 'user-section') {
+            user_section_container.classList.replace('user-section', 'user-section-disable')
+        }
         cart_items_container.classList.replace('cart-items-container-disable', 'cart-items-container')
     } else {
         cart_items_container.classList.replace('cart-items-container', 'cart-items-container-disable')
@@ -34,7 +64,6 @@ btn_cart_container.addEventListener('click', () => {
     } else {
         second_section_cart.classList.replace('second-section-cart-disable', 'second-section-cart')
         cart_empty_msj.classList.replace('cart-empty-msj', 'cart-empty-msj-disable')
-
     }
 })
 
@@ -101,13 +130,32 @@ function add_to_cart() {
         cart_empty_msj.classList.replace('cart-empty-msj', 'cart-empty-msj-disable')
         total_buy()
     } else if (amount.innerText == 0) {
-        cart_amount.className = 'cart_amount-disable'
-        cart_amount.innerText = null
-        second_section_cart.classList.replace('second-section-cart', 'second-section-cart-disable')
-        cart_empty_msj.classList.replace('cart-empty-msj-disable', 'cart-empty-msj')
+        reset_amount_cart()
     }
 }
 
+function reset_amount_cart() {
+    cart_amount.className = 'cart_amount-disable'
+    cart_amount.innerText = null
+    second_section_cart.classList.replace('second-section-cart', 'second-section-cart-disable')
+    cart_empty_msj.classList.replace('cart-empty-msj-disable', 'cart-empty-msj')
+    
+}
+
 function total_buy() {
-    cart_items_price.innerText = `$125.00 x ${amount.innerText} = $${125.00 * amount.innerText}`
+    cart_items_price.innerHTML = `: $125.00 x ${amount.innerText} = <b> $${125.00 * amount.innerText}</b>`
+}
+
+function close_alert() {
+    const msj_container_alert = document.getElementById('msj-container-alert')
+    msj_container_alert.classList.replace('msj-container-alert', 'msj-container-alert-disable')
+    cart_items_container.classList.replace('cart-items-container', 'cart-items-container-disable')
+    reset_amount_cart()
+    amount.innerText = 0
+}
+
+function open_alert() {
+    const msj_container_alert = document.getElementById('msj-container-alert')
+    msj_container_alert.classList.replace('msj-container-alert-disable', 'msj-container-alert')
+
 }
